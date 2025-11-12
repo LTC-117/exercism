0c8b29158938d280551af3b2727e2bf3e7ec6f34
LTC-117 <lucastentoni@gmail.com>
2025-11-11 22:49:50 -0300

    [cpp 'hello-world' solution]

--- cpp/hello-world/hello_world.cpp
+++ cpp/hello-world/hello_world.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 namespace hello_world {
 
-string hello() { return "Goodbye, Mars!"; }
+    string hello()
+    {
+        return "Hello, World!";
+    }
 
 }  // namespace hello_world
