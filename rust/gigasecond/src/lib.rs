fb48d17702ddca0b2e4f9af5e8f01878eecff0a9
LTC-117 <lucastentoni@gmail.com>
2025-11-13 19:25:43 -0300

    [Implementing 'gigasecond' exercise's solution]

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,8 +1,12 @@
-use time::PrimitiveDateTime as DateTime;
+use time::{Duration, PrimitiveDateTime as DateTime};
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    let expected: DateTime = DateTime::new(start.date(), start.time());
+    let mut expected: DateTime = DateTime::new(start.date(), start.time());
+
+    let gigaseconds = Duration::new(1_000_000_000, 0);
+
+    expected += gigaseconds;
 
     expected
 }
