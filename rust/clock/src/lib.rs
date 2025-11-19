a4f1eae1ef52a355106f60f6191158ec05213017
LTC-117 <lucastentoni@gmail.com>
2025-11-19 00:48:23 -0300

    [Implementing track solution (clock)]

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,11 +1,28 @@
-pub struct Clock;
+use std::fmt::{ Display, Formatter, Result, Debug };
+use std::cmp::{ PartialEq };
+
+const DAY: i64 = 24 * 60;
+const HOUR: i64 = 60;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Clock {
+    minutes: i64,
+}
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{:02}:{:02}", self.minutes / HOUR, self.minutes % HOUR)
+    }
+}
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+    pub fn new(hrs: i64, min: i64) -> Clock {
+        Clock {
+            minutes: (((hrs * HOUR + min) % DAY) + DAY) % DAY
+        }
     }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+    pub fn add_minutes(&self, min: i64) -> Clock {
+        Clock::new(0, self.minutes + min)
     }
 }
