51f70a32eca5d4d58b8818b2a77ed62f93afe1f7
LTC-117 <lucastentoni@gmail.com>
2026-01-02 16:11:03 -0300

    [Submitting exercise's solution (anagram)]

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,5 +1,32 @@
-use std::collections::HashSet;
+use std::{collections::HashSet};
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let mut anagrams: HashSet<&'a str> = HashSet::new();
+    let lower_word: String = word.to_lowercase();
+    let mut word_marker: [usize; 100_000] = [0; 100_000];
+
+    for letter in lower_word.chars() {
+        let current = letter as usize;
+        word_marker[current] += 1;
+    }
+
+    for possible in possible_anagrams {
+        let mut anag_marker: [usize; 100_000] = [0; 100_000];
+        let lower_anagram: String = possible.to_lowercase();
+
+        if lower_word == lower_anagram {
+            continue;
+        }
+
+        for letter in lower_anagram.chars() {
+            let current = letter as usize;
+            anag_marker[current] += 1;
+        }
+
+        if anag_marker == word_marker {
+            anagrams.insert(possible);
+        }
+    }
+
+    anagrams
 }
