60f9149a791691c1de0444fee8749cb6b7ed8569
LTC-117 <lucastentoni@gmail.com>
2026-01-16 15:05:19 -0300

    [Minor corrections on track solution (annalyns-infiltration)]

--- go/annalyns-infiltration/annalyns_infiltration.go
+++ go/annalyns-infiltration/annalyns_infiltration.go
@@ -2,17 +2,17 @@ package annalyn
 
 // CanFastAttack can be executed only when the knight is sleeping.
 func CanFastAttack(knightIsAwake bool) bool {
-	return !knightIsAwake;
+	return !knightIsAwake
 }
 
 // CanSpy can be executed if at least one of the characters is awake.
 func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
-	return (knightIsAwake || archerIsAwake || prisonerIsAwake);
+	return (knightIsAwake || archerIsAwake || prisonerIsAwake)
 }
 
 // CanSignalPrisoner can be executed if the prisoner is awake and the archer is sleeping.
 func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
-	return (!archerIsAwake) && prisonerIsAwake;
+	return (!archerIsAwake) && prisonerIsAwake
 }
 
 // CanFreePrisoner can be executed if the prisoner is awake and the other 2 characters are asleep
@@ -20,9 +20,9 @@ func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
 func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
 	if (!archerIsAwake) {
 		if (petDogIsPresent) {
-			return true;
+			return true
 		} else {
-			return (!knightIsAwake && prisonerIsAwake);
+			return (!knightIsAwake && prisonerIsAwake)
 		}
 	}
 
