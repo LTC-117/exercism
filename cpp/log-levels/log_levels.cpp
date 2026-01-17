12df62cdc79d9c43fc876ed2ce7ef6f37a89da69
LTC-117 <lucastentoni@gmail.com>
2026-01-17 17:32:09 -0300

    [Submitting track solution (log-levels)]

--- cpp/log-levels/log_levels.cpp
+++ cpp/log-levels/log_levels.cpp
@@ -2,11 +2,31 @@
 
 namespace log_line {
     std::string message(std::string line) {
+        std::string message;
+
+        int index = line.find(":");
+        message = line.substr(index + 2);
+
+        return message;
     }
 
     std::string log_level(std::string line) {
+        std::string log_level;
+
+        int end = line.find("]");
+        log_level = line.substr(1, end - 1);
+
+        return log_level;
     }
 
     std::string reformat(std::string line) {
+        std::string reformat;
+
+        std::string message = log_line::message(line);
+        std::string log_level = log_line::log_level(line);
+
+        reformat = message + " (" + log_level + ")";
+
+        return reformat;
     }
 }  // namespace log_line
