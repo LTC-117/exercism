58e9f0d175d22bc3ea7c2e192e9cc6a6d7bae5f3
LTC-117 <lucastentoni@gmail.com>
2026-01-17 17:12:27 -0300

    [Adding new track (log-levels)]

---
+++ cpp/log-levels/log_levels.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+namespace log_line {
+    std::string message(std::string line) {
+    }
+
+    std::string log_level(std::string line) {
+    }
+
+    std::string reformat(std::string line) {
+    }
+}  // namespace log_line
